4ad5febe91e12fc850bd5485e5064133cef33f82
Matheus Pagni <matheus.pagni@resource.com.br>
2018-08-16 16:25:33 -0300

    caguei

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 window.onload = function () {
     document.getElementById('pastelMap').addEventListener('click', function (e) {
-        document.getElementById(Math.floor(Math.random() * 7)).play();
+        document.getElementById(Math.floor(Math.random() * 8)).play();
     });
 
     document.getElementById('brejaMap').addEventListener('click', function (e) {
