79c24c71cc21551a8262af404d27ab5ef20e6a71
Matheus Pagni <matheus.pagni@resource.com.br>
2018-08-16 16:33:58 -0300

    ajustezinho

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 window.onload = function () {
+
+    var id = 0;
+
     document.getElementById('pastelMap').addEventListener('click', function (e) {
-        document.getElementById(Math.floor(Math.random() * 8)).play();
+        var idAntigo = id;
+
+        while (idAntigo == id) {
+            id = Math.floor(Math.random() * 8)
+        }
+
+        document.getElementById(id).play();
     });
 
     document.getElementById('brejaMap').addEventListener('click', function (e) {
         document.getElementById('99').play();
     });
+
+    setInterval(function () {
+        document.getElementById('pastelMap').click();
+    }, 300000);
 }
